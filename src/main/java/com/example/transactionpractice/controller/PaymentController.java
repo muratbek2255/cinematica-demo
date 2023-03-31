2019efa3c6e350f2cbd3f55fbf9a720b5a175037
muratbek2255 <muratbek2255@gmail.com>
2023-03-31 16:56:26 +0600

    fix scheduler

--- src/main/java/com/example/transactionpractice/controller/PaymentController.java
+++ src/main/java/com/example/transactionpractice/controller/PaymentController.java
@@ -3,10 +3,16 @@ package com.example.transactionpractice.controller;
 
 import com.example.transactionpractice.dto.PaymentCheckRequest;
 import com.example.transactionpractice.dto.PaymentRequest;
+import com.example.transactionpractice.entity.Payment;
+import com.example.transactionpractice.entity.PaymentStatus;
+import com.example.transactionpractice.repository.PaymentRepository;
 import com.example.transactionpractice.service.PaymentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -15,9 +21,15 @@ public class PaymentController {
 
     private final PaymentServiceImpl paymentService;
 
+    private final PaymentRepository paymentRepository;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
     @Autowired
-    public PaymentController(PaymentServiceImpl paymentService) {
+    public PaymentController(PaymentServiceImpl paymentService, PaymentRepository paymentRepository, KafkaTemplate<String, String> kafkaTemplate) {
         this.paymentService = paymentService;
+        this.paymentRepository = paymentRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
 
@@ -40,5 +52,19 @@ public class PaymentController {
     @PutMapping("/rollbackStatus/{id}")
     public ResponseEntity<String> rollbackStatus(@PathVariable int id) {
         return ResponseEntity.status(201).body(paymentService.rollbackPayment(id));
+    }
+
+    @GetMapping("/sendMsgToKafka")
+    public void sendMessageToKafka() {
+        List<Payment> payments = paymentRepository.findAll();
+
+        payments.forEach(
+                payment -> {
+                    if(payment.getStatus().equals(PaymentStatus.STATUS_PROCESS)) {
+                        kafkaTemplate.send("cinematica", "Payment id: " + payment.getId() +
+                                " and his payment status: " + payment.getStatus());
+                    }
+                }
+        );
     }
 }
